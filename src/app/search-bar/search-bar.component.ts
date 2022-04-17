6def7050c3d8d640eb523a5a30f8435fa4fca0fa
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-17 14:33:39 -0400

    Parent component access child event to use

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
   public term = '';
+  @Output() submitted = new EventEmitter<string>();
   constructor() {}
 
   ngOnInit(): void {}
   onFormSubmit(event: any) {
     event.preventDefault();
-    console.log(this.term);
+    this.submitted.emit(this.term);
   }
 }
+//to pass info from Child to Parent
+//import Output and EventEmitter
+//use generic typescript <type>
